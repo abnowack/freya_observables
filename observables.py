f695f7b1294451fc91ac5d52af904462ea0764a6
Aaron Nowack <aaronbnowack@gmail.com>
2015-03-16 14:51:56 -0400

    adding history references

--- observables.py
+++ observables.py
@@ -18,30 +18,38 @@ from mcnpy.mcnp_wrapper import *
 from mcnpy.ptrac import reader as preader
 import h5py as h5
 
-def parse_ptrac_to_hdf5(ptrac_filename, buffer_len=1000):
+def parse_ptrac_to_hdf5(ptrac_filename, ev_buffer_len=1000, h_buffer_len=1000):
     ''' save neutron and photon position, directory, energy, time '''
     with open(ptrac_filename, 'r') as ptrac:
         # parse headers and formats
         header = preader.ptrac_header(ptrac)
         input_format = preader.ptrac_input_format(ptrac)
         event_format = preader.ptrac_event_format(ptrac)
-        
+
         with h5.File('ptrac_.h5', 'w') as f:
-            f_events_g = f.create_group('events')
-            
-            dt = np.dtype([('evt', np.int), ('ipt', np.int), ('x', np.float), 
-                           ('y', np.float), ('z', np.float), ('u', np.float), 
+            dt = np.dtype([('evt', np.int), ('ipt', np.int), ('x', np.float),
+                           ('y', np.float), ('z', np.float), ('u', np.float),
                            ('v', np.float), ('w', np.float),
                            ('energy', np.float), ('time', np.float)])
-            
-            f_events_d = f_events_g.create_dataset('events', (buffer_len,), dtype=dt, maxshape=(None,), chunks=True)
+            ref_dt = h5.special_dtype(ref=h5.RegionReference)
+
+            f_history_d = f.create_dataset('history', (h_buffer_len,), dtype=ref_dt, maxshape=(None,), chunks=True)
+            f_events_d = f.create_dataset('events', (ev_buffer_len,), dtype=dt, maxshape=(None,), chunks=True)
             nevt = 0
-            buffer_count = 1
+            ev_buffer_count = 1
+            h_buffer_count = 1
+
             for i, history in enumerate(preader.parse_ptrac_events(ptrac, event_format)):
-                if (buffer_len*buffer_count - nevt - len(history.events)) < 0:
-                    buffer_count += 1
-                    f_events_d.resize((buffer_len * buffer_count,))                 
+                if (ev_buffer_len * ev_buffer_count - nevt) < len(history.events):
+                    ev_buffer_count += 1
+                    f_events_d.resize((ev_buffer_len * ev_buffer_count,))
+
+                if (h_buffer_len * h_buffer_count) <= i:
+                    h_buffer_len += 1
+                    f_history_d.resize((h_buffer_len * h_buffer_count,))
                 
+                f_history_d[i] = f_events_d.regionref[nevt:nevt+len(history.events)]
+
                 for ev in history.events:
                     f_events_d[nevt, 'evt'] = i
                     f_events_d[nevt, 'ipt'] = int(ev.ipt)
@@ -54,21 +62,23 @@ def parse_ptrac_to_hdf5(ptrac_filename, buffer_len=1000):
                     f_events_d[nevt, 'energy'] = ev.erg
                     f_events_d[nevt, 'time'] = ev.tme
                     nevt += 1
-            
+
             f_events_d.resize((nevt,))
-                    
+            f_history_d.resize((i,))
+
 if __name__ == '__main__':
-    
-#    with open('input_cards/geometry.i', 'r') as cardfile:
-#        card = cardfile.read()
-#
-#    with run_mcnp(card, cores=4) as (status, mcnp_dir):
-#        parse_ptrac_to_hdf5(mcnp_dir + '\\ptrac')
-    
+
+    with open('input_cards/geometry.i', 'r') as cardfile:
+        card = cardfile.read()
+
+    with run_mcnp(card, cores=4) as (status, mcnp_dir):
+        parse_ptrac_to_hdf5(mcnp_dir + '\\ptrac')
+
     # calculate nu
     print 'processing'
     with h5.File('ptrac_.h5', 'r') as f:
-        evts = f['events/events']
+        hist = f['history']
+        evts = f['events']
         history_index = evts[:-1, 'evt'] != evts[1:, 'evt']
         split_evts = np.array(np.split(evts, np.where(history_index)[0]))
         neutrons = split_evts * split_evts[:, 'ipt' == 1]
@@ -80,6 +90,6 @@ if __name__ == '__main__':
     plt.bar(p_bins, p_nu, color='b', alpha=0.5)
     plt.yscale('log')
     plt.show()
-    
+
     print np.average(np.arange(len(n_nu)), weights=n_nu)
     print np.average(np.arange(len(p_nu)), weights=p_nu)
