4d9b7fba9e855eea411b596de45187aa4915494b
Aaron Nowack <aaronbnowack@gmail.com>
2015-03-16 13:42:20 -0400

    converting to flat hdf5 file

--- observables.py
+++ observables.py
@@ -1,5 +1,15 @@
 """
 TODO:
+Might consider not saving in event folder, but with event field sequentially
+[ ] Finalize HDF5 Format
+[ ] Automated ways to plot
+    [ ] Avg Neutrons
+    [ ] Avg Photons
+    [ ] Time Histogram of Neutrons
+    [ ] Time Histogram of Photons
+    [ ] Angle between neutrons
+    [ ] Angle between photons
+    [ ] Feynamn Distribution
 """
 
 import numpy as np
@@ -8,7 +18,7 @@ from mcnpy.mcnp_wrapper import *
 from mcnpy.ptrac import reader as preader
 import h5py as h5
 
-def parse_ptrac_to_hdf5(ptrac_filename):
+def parse_ptrac_to_hdf5(ptrac_filename, buffer_len=1000):
     ''' save neutron and photon position, directory, energy, time '''
     with open(ptrac_filename, 'r') as ptrac:
         # parse headers and formats
@@ -16,45 +26,36 @@ def parse_ptrac_to_hdf5(ptrac_filename):
         input_format = preader.ptrac_input_format(ptrac)
         event_format = preader.ptrac_event_format(ptrac)
         
-        with h5.File('ptrac.h5', 'w') as f:
+        with h5.File('ptrac_.h5', 'w') as f:
             f_events_g = f.create_group('events')
             
-            dt = np.dtype([('x', np.float), ('y', np.float), ('z', np.float),
-                           ('u', np.float), ('v', np.float), ('w', np.float),
+            dt = np.dtype([('evt', np.int), ('ipt', np.int), ('x', np.float), 
+                           ('y', np.float), ('z', np.float), ('u', np.float), 
+                           ('v', np.float), ('w', np.float),
                            ('energy', np.float), ('time', np.float)])
-        
+            
+            f_events_d = f_events_g.create_dataset('events', (buffer_len,), dtype=dt, maxshape=(None,), chunks=True)
+            nevt = 0
+            buffer_count = 1
             for i, history in enumerate(preader.parse_ptrac_events(ptrac, event_format)):
-                f_history_g = f_events_g.create_group(str(i))
-                neutrons = []
-                photons = []
-                for ev in history.events:
-                    if ev.ipt == 1.0:
-                        neutrons.append(ev)
-                    elif ev.ipt == 2.0:
-                        photons.append(ev)
-
-                f_neutron_d = f_history_g.create_dataset('neutrons', (len(neutrons),), dtype=dt)
-                f_photon_d = f_history_g.create_dataset('photons', (len(photons),), dtype=dt)
+                if (buffer_len*buffer_count - nevt - len(history.events)) < 0:
+                    buffer_count += 1
+                    f_events_d.resize((buffer_len * buffer_count,))                 
                 
-                for j, neutron in enumerate(neutrons):
-                    f_neutron_d[j, 'x'] = neutron.xxx
-                    f_neutron_d[j, 'y'] = neutron.yyy
-                    f_neutron_d[j, 'z'] = neutron.zzz
-                    f_neutron_d[j, 'u'] = neutron.uuu
-                    f_neutron_d[j, 'v'] = neutron.vvv
-                    f_neutron_d[j, 'w'] = neutron.www
-                    f_neutron_d[j, 'energy'] = neutron.erg
-                    f_neutron_d[j, 'time'] = neutron.tme
-
-                for j, photon in enumerate(photons):
-                    f_photon_d[j, 'x'] = photon.xxx
-                    f_photon_d[j, 'y'] = photon.yyy
-                    f_photon_d[j, 'z'] = photon.zzz
-                    f_photon_d[j, 'u'] = photon.uuu
-                    f_photon_d[j, 'v'] = photon.vvv
-                    f_photon_d[j, 'w'] = photon.www
-                    f_photon_d[j, 'energy'] = photon.erg
-                    f_photon_d[j, 'time'] = photon.tme
+                for ev in history.events:
+                    f_events_d[nevt, 'evt'] = i
+                    f_events_d[nevt, 'ipt'] = int(ev.ipt)
+                    f_events_d[nevt, 'x'] = ev.xxx
+                    f_events_d[nevt, 'y'] = ev.yyy
+                    f_events_d[nevt, 'z'] = ev.zzz
+                    f_events_d[nevt, 'u'] = ev.uuu
+                    f_events_d[nevt, 'v'] = ev.vvv
+                    f_events_d[nevt, 'w'] = ev.www
+                    f_events_d[nevt, 'energy'] = ev.erg
+                    f_events_d[nevt, 'time'] = ev.tme
+                    nevt += 1
+            
+            f_events_d.resize((nevt,))
                     
 if __name__ == '__main__':
     
@@ -66,17 +67,17 @@ if __name__ == '__main__':
     
     # calculate nu
     print 'processing'
-    with h5.File('ptrac.h5', 'r') as f:
-        n_nu = np.zeros(35)
-        p_nu = np.zeros(35)
-        for name, event in f['events'].iteritems():
-            n_n = event['neutrons'].size
-            n_p = event['photons'].size
-            n_nu[n_n] += 1
-            p_nu[n_p] += 1
+    with h5.File('ptrac_.h5', 'r') as f:
+        evts = f['events/events']
+        history_index = evts[:-1, 'evt'] != evts[1:, 'evt']
+        split_evts = np.array(np.split(evts, np.where(history_index)[0]))
+        neutrons = split_evts * split_evts[:, 'ipt' == 1]
+        photons = split_evts * split_evts[:, 'ipt' == 2]
+        n_nu, n_bins = np.histogram((np.size(n) for n in neutrons), bins=50, range=[0,50])
+        p_nu, p_bins = np.histogram((np.size(n) for n in neutrons), bins=50, range=[0,50])
 
-    plt.bar(np.arange(len(n_nu)), n_nu, color='r', alpha=0.5)
-    plt.bar(np.arange(len(p_nu)), p_nu, color='b', alpha=0.5)
+    plt.bar(n_bins, n_nu, color='r', alpha=0.5)
+    plt.bar(p_bins, p_nu, color='b', alpha=0.5)
     plt.yscale('log')
     plt.show()
     
